aeff723b3afc607a06161a5f6347298d1cbd51e3
David Townsend <dtownsend@velti.com>
2012-11-27 23:06:16 +0000

    Console map renderer

---
+++ lib/map_presenter.rb
@@ -0,0 +1,25 @@
+class MapPresenter
+
+  attr_accessor :map
+
+  def initialize(map)
+    @map = map
+  end
+
+  def to_console
+    puts "GAME GRID"
+    puts "____________________"
+    game_grid = @map.game_grid
+    10.times do |x|
+      10.times do |y|
+        print game_grid[x][y] + '|'
+      end
+      puts ""
+      print "____________________"
+      puts ""
+    end
+    nil
+  end
+
+end
+
