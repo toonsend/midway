cdbeadd5025e7dfa2aa1643a7d1b9ddb46d72298
Sergio Marques <sergio.rtmarques@gmail.com>
2012-12-20 17:17:07 +0000

    WIP: Added code to initialise a game or play a move

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -18,25 +18,57 @@ describe GameController do
         response.status.should == 200
         res = JSON::parse(response.body)
         res["error_code"].should == "INVALID_API_KEY"
-        res["message"].should == "The api key does not match the team_id"        
+        res["message"].should == "The api key does not match the team_id"
       end
 
     end
 
     context "with valid api key" do
-      
+
       before(:each) do
         @user = FactoryGirl.create(:user)
         @user.generate_api_key!
       end
 
-      it "can make move!" do
+      it "should return a NO_GAME error if there's no maps to play against" do
         request.env['HTTP_MIDWAY_API_KEY'] = @user.api_key
-        post :create, :team_id => @user.id, :move =>[200, 100]
-        response.status.should == 200
+        post :create, :team_id => @user.id, :move => [200, 100]
+        response.status.should == 422
         res = JSON::parse(response.body)
-        res["error_code"].should == "OUT_OF_RANGE"
-        res["message"].should == "move is outside of map"
+        res["error_code"].should == "NO_GAME"
+        res["message"].should == "There is currently no game to play"
+      end
+
+      it "should return a NO_MAPS_UPLOADED error if the user hasn't uploaded a map yet" do
+        FactoryGirl.create(:map, :team_id => 2)
+        request.env['HTTP_MIDWAY_API_KEY'] = @user.api_key
+        post :create, :team_id => @user.id, :move => [200, 100]
+        response.status.should == 422
+        res = JSON::parse(response.body)
+        res["error_code"].should == "NO_MAPS_UPLOADED"
+        res["message"].should == "Your team does not have any maps so can not enter tournament"
+      end
+
+      it "should return an INVALID_MOVE error if an invalid move is submitted" do
+        FactoryGirl.create(:map, :team_id => @user.id)
+        FactoryGirl.create(:map, :team_id => 2)
+        request.env['HTTP_MIDWAY_API_KEY'] = @user.api_key
+        post :create, :team_id => @user.id, :move => [200, [100]]
+        response.status.should == 422
+        res = JSON::parse(response.body)
+        res["error_code"].should == "INVALID_MOVE"
+        res["message"].should == "Invalid move"
+      end
+
+      it "should return the move state if the move is valid" do
+        FactoryGirl.create(:map, :team_id => @user.id)
+        FactoryGirl.create(:map, :team_id => 2)
+        request.env['HTTP_MIDWAY_API_KEY'] = @user.api_key
+        post :create, :team_id => @user.id, :move => [0, 0]
+        response.status.should == 422
+        res = JSON::parse(response.body)
+        res["opponent_id"].should == "2"
+        res["move"].should == "[0,0]"
       end
 
     end
