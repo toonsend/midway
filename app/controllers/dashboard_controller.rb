b454090b88d44558b21d44727d268cbef55a2b5a
Sergio Marques <sergio.rtmarques@gmail.com>
2012-12-20 17:15:19 +0000

    WIP: Refactored Key view to provide the ability to create a team and when the team is created the user is able to send invites to other users to join the team
    NOTE: missing specs to do basic validations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,13 @@ class DashboardController < ApplicationController
   end
 
   def api
-    @api_key = current_user.get_api_key
-    @team_id = current_user.id
+    if current_user.team
+      @api_key = current_user.team.get_api_key
+      @team_id = current_user.team.id
+    else
+      @api_key = "<API_KEY>"
+      @team_id = ":team_id"
+    end
     readme   = File.open('README.md')
     template = ERB.new(readme.read).result(binding)
     options  = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
@@ -14,8 +19,13 @@ class DashboardController < ApplicationController
   end
 
   def key
-    @user_id = current_user.id
-    @key     = current_user.get_api_key
+    if current_user.team
+      @team = current_user.team
+      @invite = current_user.team.invites.build(params[:invite])
+      @users = current_user.team.users_to_invite(current_user)
+    else
+      @team = Team.new
+    end
   end
 
 end
