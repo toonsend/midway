15c09fecadabe6d1216049a48174f619c641cdf2
David Townsend <dtownsend@velti.com>
2013-01-14 01:32:50 +0000

    Join model for tournament and team

--- app/models/tournament_team.rb
+++ app/models/tournament_team.rb
@@ -4,8 +4,18 @@ class TournamentTeam < ActiveRecord::Base
   belongs_to :tournament
   belongs_to :team
 
-  scope :active_tournament, lambda { |team_id|
-    includes(:team, :tournament).where(["team_id = ? and tournaments.state != 'complete'", team_id])
-  }
+  def self.active_tournament(team)
+    tournament_team = includes(:team, :tournament)
+    .where(["team_id = ? and tournaments.state != 'complete'", team.id])
+    .first
+    tournament_team.nil? ? nil : tournament_team.tournament
+  end
+
+  def self.in_progress_tournament(team)
+    tournament_team = includes(:team, :tournament)
+    .where(["team_id = ? and tournaments.state = 'in_progress'", team.id])
+    .first
+    tournament_team.nil? ? nil : tournament_team.tournament
+  end
 
 end
