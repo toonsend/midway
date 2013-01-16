a0b5ed319e8f1dda2b4d0d58c2b7956875a033f5
Sergio Marques <sergio.rtmarques@gmail.com>
2013-01-16 10:59:46 +0000

    Added index action to teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,15 @@
 class TeamsController < ApplicationController
 
+  def index
+    if current_user.team
+      @team = current_user.team
+      @invite = current_user.team.invites.build(params[:invite])
+      @users = current_user.team.users_to_invite(current_user)
+    else
+      @team = Team.new
+    end
+  end
+
   def create
     current_user.create_team(params[:team])
     current_user.save
